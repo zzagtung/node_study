18b5b5ed026ce1ad86fe6fde2089bed7c114880a
ryan <zzagtung@gmail.com>
2011-12-13 22:57:21 +0900

    POST request handle

--- requestHandlers.js
+++ requestHandlers.js
@@ -1,6 +1,6 @@
-var exec = require("child_process").exec;
+var querystring = require("querystring");
 
-function start(response) {
+function start(response, postData) {
   console.log("Request handler 'start' was called.");
   var body = '<html>'+
     '<head>'+
@@ -19,10 +19,10 @@ function start(response) {
   response.end();
 }
 
-function upload(response) {
+function upload(response, postData) {
   console.log("Request handler 'upload' was called.");
   response.writeHead(200, {"Content-Type": "text/plain"});
-  response.write("Hello Upload");
+  response.write("You've sent the text: " + querystring.parse(postData).text);
   response.end();
 }
 
--- server.js
+++ server.js
@@ -3,10 +3,19 @@ var url = require("url");
 
 function start(route, handle) {
     function onReqeust(request, response) {
+    	var postData = "";
         var pathname = url.parse(request.url).pathname;
         console.log("Reqeust for " + pathname + " received.");
+
+        request.setEncoding("utf8");
         
-        route(handle, pathname, response);
+        request.addListener("data", function(postDataChunk) {
+        	postData += postDataChunk;
+        	console.log("Received POST data chunk '" + postDataChunk + "'.");
+        });
+        request.addListener("end", function() {
+        	route(handle, pathname, response, postData);
+        });
     }
     
     http.createServer(onReqeust).listen("8888");
